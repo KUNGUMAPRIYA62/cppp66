343fe01dc7f30a2a6fd2bdc57e52cd0c7a608f9d
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-21 16:53:39 +0530

    Update swap2num.C

--- swap2num.C
+++ swap2num.C
@@ -1,6 +1,7 @@
 #include<stdio.h>
 void main()
 {
+  {
 int a,b,temp;
 printf("enter two numbers:");
 scanf("%d%d",&a,&b);
@@ -16,5 +17,6 @@ b=temp;
 printf("\nAfter swap");
 printf("\n %d",a);
 printf(" %d",b);
+}
 }
 }
