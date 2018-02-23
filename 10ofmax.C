f7b52754c7ae234d8fa9a2da8ae33485a3f09f19
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-23 15:48:31 +0530

    10ofmax.C
    max of 10

--- 10ofmax.C
+++ 10ofmax.C
@@ -6,10 +6,8 @@ int n,i,max=array[0];
 printf("enter the number of elements:");
 scanf("%d",&n);
 printf("\n enter the array elements:");
-for(i=0;i<n;i++)
-{
 scanf("%d",&array[i]);
-}
+  {
 for(i=0;i<n;i++)
 {
 if(array[i]>max)
@@ -18,4 +16,5 @@ max=array[i];
 }
 }
 printf("\nthe maximum of the numbers is:%d",max);
+}
 }
