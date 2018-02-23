c325a08a131b0552194020df3186fd11b7e0cae7
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-23 15:35:36 +0530

    10ofmax.C
    maximum of 10 num

---
+++ 10ofmax.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e,f,g,h,i,j,max;
+printf("enter ten numbers");
+scanf("%d",&ten numbers);
+{
+for(i=0;i<10;i++);
+}
+printf("maximum among all ten numbers=%d",max);
+}
