b57c45309e5fe978adbee32d4406d806cdac888d
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-28 16:28:41 +0530

    divby7.C
    divisible by 7

---
+++ divby7.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter a number:");
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("yes..multiple of 7");
+    }
+    else
+    {
+        printf("no");   
+    }
+}
