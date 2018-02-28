792f010b6bed2427b7d0baaf9e544cd625f2f201
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-28 15:57:30 +0530

    prime1.C
    prime or not

---
+++ prime1.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+int a;
+printf("Enter the number:");
+scanf("%d",&a);
+if((a/1)||(a/a))
+{
+  printf("prime number");
+}
+ else
+ {
+ printf("not a prime number");
+}
+}
