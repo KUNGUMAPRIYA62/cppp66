1b82f9890179b4190dd1ed2ca3b4801eb1060dec
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-03-01 20:12:25 +0530

    strpalindrome.C
    palindrome

---
+++ strpalindrome.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+    char a[100];
+    int n,i,count=0;
+    scanf("%s",a);
+    n=strlen(a);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==a[n-i-1])
+        {
+            count++;
+        }
+    }
+    if(n==count)
+    {
+        printf("Palindrome");
+    }
+    else
+    {
+        printf("not palindrome");
+    }
+}
